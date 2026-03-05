5c513915f674b96de703ee579875cff31f62c77f
ZEN4ai <dimonlox502@gmail.com>
2026-03-05 09:34:18 +0200

    fix redstoneWatcher

--- other_stuff/redstoneWatcher.lua
+++ other_stuff/redstoneWatcher.lua
@@ -1,19 +1,23 @@
 -- config
 -- wget https://raw.githubusercontent.com/ZEN4ai/cc-tweaked-relay-controller/other_stuff/other_stuff/redstoneWatcher.lua
 
-local container = "right"     -- container side or peripheral name
-local redstoneSide = "top" -- redstone output side
+local container = "left"     -- container side or peripheral name
+local redstoneSide = "right" -- redstone output side
 
 local inv = peripheral.wrap(container)
 
 while true do
-    redstone.setOutput(redstoneSide, false)
+    local hasItem = false
+
     local items = inv.list()
     for _, item in pairs(items) do
         if item then
-            redstone.setOutput(redstoneSide, true)
+            hasItem = true
             break
         end
     end
+
+    redstone.setOutput(redstoneSide, hasItem)
+
     os.sleep(0.5)
 end
